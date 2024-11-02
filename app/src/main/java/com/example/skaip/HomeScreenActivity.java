176c61f39286a05a6b1dacf2bce67264ae948b4b
gustavsoto <gustavs.oto.cers@gmail.com>
2024-11-02 20:44:37 +0200

    Working sign in (Homescreen kinda screwed me big time with setContentView)

--- app/src/main/java/com/example/skaip/HomeScreenActivity.java
+++ app/src/main/java/com/example/skaip/HomeScreenActivity.java
@@ -4,23 +4,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class HomeScreenActivity extends AppCompatActivity {
+    private Preferences preferences;
+    private TextView top_panel_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.home_screen);
-        Button backButton = findViewById(R.id.back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        ImageButton logOut = findViewById(R.id.logout);
+        top_panel_text = findViewById(R.id.top_panel_text);
+        ImageView user_icon = findViewById(R.id.user_icon);
+        preferences = new Preferences(getApplicationContext());
+        loadUserData();
+        logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeScreenActivity.this, WelcomeScreenActivity.class);
+                Intent intent = new Intent(getApplicationContext(), WelcomeScreenActivity.class);
                 startActivity(intent);
             }
         });
+    }
+    private void loadUserData(){
+        top_panel_text.setText(preferences.getString(Constants.KEY_NAME));
+    }
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateFcmToken);
+    }
+    private void updateFcmToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database
+                .collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferences.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> Toast.makeText(getApplicationContext(), "Token successfully updated", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(exception -> Toast.makeText(getApplicationContext(), "Error updating token", Toast.LENGTH_SHORT).show());
     }
 }
