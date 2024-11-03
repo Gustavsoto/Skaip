7182628c421ae9857b3af8f801ff108cbe72a177
gustavsoto <gustavs.oto.cers@gmail.com>
2024-11-03 13:39:07 +0200

    Create group added

--- app/src/main/java/com/example/skaip/Constants.java
+++ app/src/main/java/com/example/skaip/Constants.java
@@ -3,6 +3,7 @@ package com.example.skaip;
 public class Constants {
     //konstantes lietotajam
     public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_COLLECTION_GROUPS = "groups";
     public static final String KEY_NAME = "name";
     public static final String KEY_COURSE = "course";
     public static final String KEY_YEAR = "year";
@@ -12,4 +13,8 @@ public class Constants {
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
     public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
     public static final String KEY_FCM_TOKEN = "fcmToken";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_GROUP_NAME = "groupName";
+    public static final String KEY_GROUP_DESCRIPTION = "groupDescription";
+    public static final String KEY_GROUP_FILE = "groupFile";
 }
